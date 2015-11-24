69203965faadf3194e2ed1a0ecf44489cefc1d94
Anna <annachem368@gmail.com>
2015-11-24 16:10:14 -0600

    add garagedoorOpen in client

--- src/command_pattern/RemoteControlTest.java
+++ src/command_pattern/RemoteControlTest.java
@@ -1,12 +1,24 @@
 package command_pattern;
 
+/**
+ * This is our Client in Command Pattern
+ * 
+ * @author mac
+ *
+ */
 public class RemoteControlTest {
 	public static void main(String[] args) {
 		SimpleRemoteControl remote = new SimpleRemoteControl();
 		Light light = new Light();
+		GarageDoor garageDoor = new GarageDoor();
+		
 		LightOnCommand lightOn = new LightOnCommand(light);
+		GarageDoorOpenCommand garageOpen = new GarageDoorOpenCommand(garageDoor);
 		
 		remote.setCommand(lightOn);
+		remote.buttonWasPressed();
+		
+		remote.setCommand(garageOpen);
 		remote.buttonWasPressed();
 	}
 }
